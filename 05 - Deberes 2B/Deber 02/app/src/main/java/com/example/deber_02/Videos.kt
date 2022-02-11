fcedfbfb0d0b3509080b88f80daf498b9d7ff2ce
Carlos Calva <carlos.calva@epn.edu.ec>
2022-02-11 16:46:06 -0500

    update / Deber 02 - TikTok

---
+++ 05 - Deberes 2B/Deber 02/app/src/main/java/com/example/deber_02/Videos.kt
@@ -0,0 +1,45 @@
+package com.example.deber_02
+
+import android.os.Parcel
+import android.os.Parcelable
+
+class Videos(
+    val enlace: String?,
+    val usuario: String?,
+    val hashtag: String?,
+    val musica: String?
+): Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString()
+    ) {
+
+    }
+
+    override fun toString(): String{
+        return "${enlace}-${usuario}-${hashtag}-${musica}"
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+     parcel.writeString(enlace)
+        parcel.writeString(usuario)
+        parcel.writeString(hashtag)
+        parcel.writeString(musica)
+    }
+
+    companion object CREATOR : Parcelable.Creator<Videos> {
+        override fun createFromParcel(parcel: Parcel): Videos {
+            return Videos(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Videos?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
---
+++ 06 - Firebase/app/src/main/java/com/example/proyectofirebase/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.proyectofirebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val botonLogin= findViewById<Button>(R.id.btn_login)
+
+
+
+    }
+}
